3e74498990c4fbf49a2dc012bf67bad08059af25
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-04-16 00:10:12 +0530

    Added Routes

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  // mongoose.connect will return a promise so we use async/await in a try catch block
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failiure
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
 // Initialize app variable with express
 const app = express();
+// connect Database
+connectDB();
+
 // create a single endpoint
 app.get("/", (req, res) => res.send("API Running"));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 // If no env variable default the server to 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
