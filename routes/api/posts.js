3d0df455281db5ad5caa86af6ffaa5871f8bcbdd
Nithin Samuel <nithin.thewebdev@gmail.com>
2021-04-27 21:08:04 +0530

    Added Like and Unlike functionality to posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -104,4 +104,70 @@ router.delete("/:id", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+// @route   PUT api/posts/like/:id
+// @desc    Like a Post
+// @access  Private
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    // find the post by id
+    const post = await Post.findById(req.params.id);
+    // check if the post has already been liked by the user
+    // compare current iteration of the user to the user that is logged in
+    //console.log("req.user--from middleware auth", req.user);
+    //req.user--from middleware auth, { id: '6083ceca38d8734c1884c6c7' }
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Post Already Liked" });
+    }
+    // if post is not already liked by the user, add the user to the likes array
+    post.likes.unshift({ user: req.user.id });
+    // save it to db
+    await post.save();
+    // return response with likes
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+// @route   PUT api/posts/unlike/:id
+// @desc    Unlike a Post
+// @access  Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    // find the post by id
+    const post = await Post.findById(req.params.id);
+    // check if the post has already been liked by the user--for unliking user id should not be present in likes array so filter check should not return anything
+    // compare current iteration of the user to the user that is logged in
+    //console.log("req.user--from middleware auth", req.user);
+    //req.user--from middleware auth, { id: '6083ceca38d8734c1884c6c7' }
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: "Post Has Not Yet Been Liked!" });
+    }
+    // get Remove Index to remove from the liked array
+    //find the index of like to be removed by the user who liked it
+    const removeIndex = post.likes
+      .map((like) => {
+        // console.log("like", like);
+        //like { _id: 60882bbffab16d63b03909e6, user: 6083d2d638d8734c1884c6ca }
+        return like.user.toString();
+      })
+      .indexOf(req.user.id);
+    // splice the post to be removed from the array
+    post.likes.splice(removeIndex, 1);
+    // save it to db
+    await post.save();
+    // return response with likes
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
